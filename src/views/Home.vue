f26d602073dbe8260cf79e927bd788f551f8f35a
ilfat <test@example.com>
2021-04-09 13:49:27 +0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="home">
+    <h1>Доброе утро</h1>
+    <div>
+      <img src="../assets/img/book.svg" />
+    </div>
     <div class="nav">
       <div>
-        <router-link to="/workplace">Продолжить локально</router-link>
+        <router-link to="/workplace" class="button  button-outline">Продолжить локально</router-link>
       </div>
       <div>
-        <router-link to="/workplace/new">Новый файл</router-link>
+        <router-link to="/workplace/new" class="button  button-outline">Новый файл</router-link>
       </div>
       <div>
-        <p  @click="showModal = true" data-noclose="true">Открыть по ID</p>
+        <p @click="showModal = true" class="button  button-outline" data-noclose="true">Открыть по ID</p>
       </div>
       <div>
-        <p @click="loadFile">Загрузить файл</p>
+        <p @click="loadFile" class="button button-outline">Загрузить файл</p>
       </div>
     </div>
     <transition name="fade">
-      <InputModal  v-click-outside="close" @loadById="loadById" v-show="showModal"/>
+      <InputModal v-click-outside="close" @loadById="loadById" v-show="showModal" />
     </transition>
   </div>
 </template>
@@ -70,3 +74,8 @@ export default {
   },
 };
 </script>
+<style>
+  h1{
+    text-align: center;
+  }
+</style>
